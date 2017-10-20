439b05f87f2a6106e0488bd9cd167a45d48d4b76
Cerbrus <Cerbrus@users.noreply.github.com>
2017-10-20 08:57:42 +0200

    Update BatNaN.js

--- BatNaN.js
+++ BatNaN.js
@@ -1,1 +1,3 @@
 new Array(16).join(Math.sqrt(-1)).replace(new RegExp((-{}+[])[0] + '(' + (([]/[])+[])[0] + ')', 'g'), ' $1').slice(0, 44) + String.fromCharCode(64^96, 66&66, 1+8**2+(96^64), parseInt("1" + (-~[]<<4), 10)) + ([]*{}) + '!'
+new Array(16).join(Math.sqrt(-1)).replace(new RegExp((-{}+[])[0] + '(' + (([]/[])+[])[0] + ')', 'g'), ' $1').slice(0, 44) + String.fromCharCode(64^96, 66&66, 1+8**2+(96^64), +(+!false + '' + (-~[]<<4))) + ([]*{}) + '!'
+new Array(16).join([]/[]).replace(/NN/g, ' N').slice(0, 44) + ' Bat' + ([]*{}) + '!'

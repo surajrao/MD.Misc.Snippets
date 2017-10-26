ad46669017a69807c01bc19c935bcf8db9d8e22c
Michiel <mdommerholt@mypoiworld.com>
2017-10-26 09:10:52 +0200

    Adding bat snippets

---
+++ bat/FirewallBlockAllInThisFolder.bat
@@ -0,0 +1,8 @@
+﻿REM Block all exe files in this folder, in the Windows Firewall
+
+@ setlocal enableextensions 
+@ cd /d "%~dp0"
+
+for /R %%a in (*.exe) do (
+    netsh advfirewall firewall add rule name="Blocked with Batchfile %%a" dir=out program="%%a" action=block
+)
